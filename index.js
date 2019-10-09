1646db9c8cd267fd774a33691a94547525dd34e8
Santithon <santithon@DEVICEs-MacBook-Pro.local>
2019-10-09 14:38:00 +0700

    Update lodash

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const moment = require('moment')
+const _ = require('lodash')
 const app = express()
 const port = 3000
 
 //app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', (req, res) => { 
     const currentDateTime = moment()
-    res.send(`Hello World!<br/>${currentDateTime.format()}`)
+    const content = `Hello World!<br/>${currentDateTime.format()}`
+    res.send(_.toUpper(content))
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
