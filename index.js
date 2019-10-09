8b86672d25294a24ed6a4016bb01bd95fde2a2f1
Santithon <santithon@DEVICEs-MacBook-Pro.local>
2019-10-09 14:05:58 +0700

    update moment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const moment = require('moment')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => { 
+    const currentDateTime = moment()
+    res.send(`Hello World!<br/>${currentDateTime.format()}`)
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
